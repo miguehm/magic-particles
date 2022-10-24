79e440ab776d5999156552f69d7400d8b07fd478
miguehm <miguel.hernandezmor@alumno.buap.mx>
2022-10-23 23:32:08 -0500

    ADD: Formula para crear notas, Comentarios

--- main.js
+++ main.js
@@ -2,29 +2,53 @@
 
 import * as THREE from 'three';
 
+// Creamos el audio context
+// será el destino del audio
 const audioContext = new AudioContext();
-const buffer = audioContext.createBuffer(1, audioContext.sampleRate * 1, audioContext.sampleRate);
 
+// objeto buffer genérico
+// se almacenan los datos del sonido
+const buffer = audioContext.createBuffer(
+	1, // audio mono
+	audioContext.sampleRate * 1, 
+	audioContext.sampleRate
+);
+
+// Se trabaja con el primer canal de audio
 const channelData = buffer.getChannelData(0)
 
+// Datos del primer canal
 for (let i = 0; i < buffer.length; i++){
 	channelData[i] = Math.random() * 2 - 1
 }
 
+// modulo que modifica la ganancia
 const primaryGainControl = audioContext.createGain()
 primaryGainControl.gain.setValueAtTime(0.06, 0)
+// conecta la ganancia al nodo destino del audio context
 primaryGainControl.connect(audioContext.destination)
 
 const button = document.createElement('button')
 button.innerText = 'Sonido blanco'
+
 button.addEventListener('click', () => {
+	// creando un buffer fuente
 	const whiteNoiseSource = audioContext.createBufferSource();
+	// toma el objeto del buffer genérico
 	whiteNoiseSource.buffer = buffer;
+	// conecta con el control de ganancia
 	whiteNoiseSource.connect(primaryGainControl)
+	// reproduce el sonido
 	whiteNoiseSource.start()
 })
 
 document.body.appendChild(button)
+// nodo de conexión
+// .  whiteNoiseSource
+// -> primaryGainControl
+// -> audioContext.destination
+
+// 
 
 const snareFilter = audioContext.createBiquadFilter()
 snareFilter.type = "highpass"
@@ -43,18 +67,32 @@ snareButton.addEventListener('click', () => {
 document.body.appendChild(snareButton)
 
 // piano
+
+const la4 = 440.0
+const r = 1.059463
+
 const kickButton = document.createElement('button')
 kickButton.innerText = 'Kick'
 kickButton.addEventListener('click', () => {
+	// crear oscilador para emular la frecuencia de sonido de las notas
 	const kickOscillator = audioContext.createOscillator()
-
-	kickOscillator.frequency.setValueAtTime(523.2, 0)
-	kickOscillator.type = 'square'
+	// pasar un array de todas las posibles oscilaciones de las notas de piano?
+	const note = la4 * Math.pow(r, 3) //la5#
+	kickOscillator.frequency.setValueAtTime(note, 0)
+	// tipo de onda del oscilador
+	kickOscillator.type = 'sine'
 	kickOscillator.connect(primaryGainControl)
 	kickOscillator.start()
+	// detiene el oscilador despues de 0.5 segundos
 	kickOscillator.stop(audioContext.currentTime + 0.5)
 })
+
 document.body.appendChild(kickButton)
+
+// nodos
+// .  kickOscillator
+// -> primaryGainControl
+// -> audioContext.destination
 
 // tambor
 // const kickButton = document.createElement('button')
