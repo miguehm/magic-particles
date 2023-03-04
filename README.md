3bc687a38ca0f38a8bae0a4713070f9e8a84a217
miguehm <miguel.hernandezmor@alumno.buap.mx>
2023-03-04 12:01:58 -0600

    add: some comments; rework: README

--- README.md
+++ README.md
@@ -1,9 +1,38 @@
-An Audiocontext example.
+# Magic Particles
 
-# How to run
-1. Clone this repo.
-2. Go to *synt-threejs* folder.
-3. Install all dependences with `npm install`.
-4. Run via `npm run dev`.
+Image drawing with particles appearing using music (battery kicks). First steps using `Web Audiocontext`.
 
+img
+
+## How to run
+
+Install npm dependences.
+
+```bash
+npm install
+```
+
+Run project with `vite`.
+
+```bash
+npm run dev
+```
 > Working on Nodejs 18.7.0 and npm 8.15
+
+## Modes
+
+### Rubik
+
+Is the first animation can you see.
+
+img
+
+### Butterfly
+
+It is located behind the rubik animation. You can use your mouse for move between both images.
+
+img
+
+# References
+
+Particles information created by [Geometrize](https://www.geometrize.co.uk/).
--- main.js
+++ main.js
@@ -35,6 +35,7 @@ var bufferLength = analyser.frequencyBinCount;
 var dataArray = new Uint8Array(bufferLength);
 console.log(dataArray);
 
+// loading particles info files
 const mariposaRes = await fetch('mariposa.json');
 const mariposaData = await mariposaRes.json();
 
@@ -63,6 +64,7 @@ function createBg(data){
 	return bg;
 }
 
+// particle creation
 function createCircle(data, step, zFight, x, y, z, rx, ry, rz){
 	const colorC = 'rgb('
 		+data['shapes'][step]['color'][0]+', '
@@ -91,11 +93,11 @@ function createCircle(data, step, zFight, x, y, z, rx, ry, rz){
 		})
 	);
 
-	// Rotar imagen acorde a un ángulo
+	// rotate image
 	ry = (ry*Math.PI)/180;
 	circle.rotation.y = ry;
 	
-	// Matriz de transformación - rotationY
+	// transformation matrix - y-axis
 	circle.position.x = pos.x*Math.cos(ry) + pos.z*Math.sin(ry);
 	circle.position.y = pos.y;
 	circle.position.z = -pos.x*Math.sin(ry)+pos.z*Math.cos(ry);
@@ -138,7 +140,7 @@ class Figure {
 			this.rz
 		);
 
-		// Arreglo de todos los objs circle
+		// circle objs array
 		this.objects.push(circle);
 		this.step += 1;
 
@@ -249,119 +251,3 @@ function animate(){
 }
 
 animate();
-
-
-// ================ AUDIO ===============
-/*
-// Creamos el audio context
-// será el destino del audio
-const audioContext = new AudioContext();
-
-// objeto buffer genérico
-// se almacenan los datos del sonido
-const buffer = audioContext.createBuffer(
-	1, // audio mono
-	audioContext.sampleRate * 1, 
-	audioContext.sampleRate
-);
-
-// Se trabaja con el primer canal de audio
-const channelData = buffer.getChannelData(0)
-
-// Datos del primer canal
-for (let i = 0; i < buffer.length; i++){
-	channelData[i] = Math.random() * 2 - 1
-}
-
-// modulo que modifica la ganancia
-const primaryGainControl = audioContext.createGain()
-primaryGainControl.gain.setValueAtTime(0.06, 0)
-// conecta la ganancia al nodo destino del audio context
-primaryGainControl.connect(audioContext.destination)
-
-const button = document.createElement('button')
-button.innerText = 'Sonido blanco'
-
-button.addEventListener('click', () => {
-	// creando un buffer fuente
-	const whiteNoiseSource = audioContext.createBufferSource();
-	// toma el objeto del buffer genérico
-	whiteNoiseSource.buffer = buffer;
-	// conecta con el control de ganancia
-	whiteNoiseSource.connect(primaryGainControl)
-	// reproduce el sonido
-	whiteNoiseSource.start()
-})
-
-// document.body.appendChild(button)
-
-const snareFilter = audioContext.createBiquadFilter()
-snareFilter.type = "highpass"
-snareFilter.frequency.value = 1500
-snareFilter.connect(primaryGainControl)
-
-const snareButton = document.createElement('button')
-snareButton.innerText = 'Snare'
-snareButton.addEventListener('click', () => {
-	const whiteNoiseSource = audioContext.createBufferSource()
-	whiteNoiseSource.buffer = buffer
-	whiteNoiseSource.connect(snareFilter)
-	whiteNoiseSource.start()
-})
-
-// document.body.appendChild(snareButton)
-
-// piano
-
-const la4 = 440.0
-const r = 1.059463
-
-const kickButton = document.createElement('button')
-kickButton.innerText = 'Kick'
-kickButton.addEventListener('click', () => {
-	// crear oscilador para emular la frecuencia de sonido de las notas
-	const kickOscillator = audioContext.createOscillator()
-	// pasar un array de todas las posibles oscilaciones de las notas de piano?
-	const note = la4 * Math.pow(r, 3) //la5#
-	kickOscillator.frequency.setValueAtTime(note, 0)
-	// tipo de onda del oscilador
-	kickOscillator.type = 'sine'
-	kickOscillator.connect(primaryGainControl)
-	kickOscillator.start()
-	// detiene el oscilador despues de 0.5 segundos
-	kickOscillator.stop(audioContext.currentTime + 0.5)
-})
-// document.body.appendChild(kickButton)
-
-// nodos
-// .  kickOscillator
-// -> primaryGainControl
-// -> audioContext.destination
-
-// tambor
-// const kickButton = document.createElement('button')
-// kickButton.innerText = 'tambor'
-// kickButton.addEventListener('click', () => {
-// 	const kickOscillator = audioContext.createOscillator()
-// 
-// 	kickOscillator.frequency.setValueAtTime(500, 0)
-// 	kickOscillator.frequency.exponentialRampToValueAtTime(
-// 		0.001,
-// 		audioContext.currentTime + 0.5
-// 	)
-// 
-// 	const kickGain = audioContext.createGain()
-// 	kickGain.gain.setValueAtTime(1, 0)
-// 	kickGain.gain.exponentialRampToValueAtTime(
-// 		0.001,
-// 		audioContext.currentTime + 0.5
-// 	)
-// 
-// 	kickOscillator.connect(kickGain)
-// 	kickGain.connect(primaryGainControl)
-// 	kickOscillator.start()
-// 	kickOscillator.stop(audioContext.currentTime + 0.5)
-// })
-// 
-// document.body.appendChild(kickButton)
-*/
