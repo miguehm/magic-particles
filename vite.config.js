94717d8ca5f8368823fedcac7c4ceb7889c4fc99
miguehm <miguel.hernandezmor@alumno.buap.mx>
2023-03-04 23:23:07 -0600

    fix: await issue

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from "vite";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
-	base: 'magic-particles'
+	base: 'magic-particles',
+	plugins: [
+		topLevelAwait({
+		  // The export name of top-level await promise for each chunk module
+		  promiseExportName: "__tla",
+		  // The function to generate import names of top-level await promise in each chunk module
+		  promiseImportName: i => `__tla_${i}`
+		})
+	]
 })
